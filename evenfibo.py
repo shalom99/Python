35eabcd7b91bd3b0828ef7367d603e42e597fa5d
M304-19 <M304-19@M304-19s-iMac.local>
2017-06-16 17:30:23 +0800

    Hello World

--- evenfibo.py
+++ evenfibo.py
@@ -1,2 +1,12 @@
 
-def main():
+def SubFib(startNumber, endNumber):
+    n = 0
+    cur = f(n)
+    while cur <= endNumber:
+        if startNumber <= cur:
+            print cur
+        n += 1
+        cur = f(n)
+
+answer = SubFib(0,4000000)
+print answer
